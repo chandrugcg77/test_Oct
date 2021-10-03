bd80591db267fc54b4a174c434912326027b5aa0
Chandrasekar A <47851967+chandrugcg77@users.noreply.github.com>
2021-10-03 21:40:22 +0530

    Create list_indexing_slicing.py

---
+++ list_indexing_slicing.py
@@ -0,0 +1,9 @@
+#indexing
+
+#              -4       -3         -2             -1           #RIGHT-LEFT
+sequences=["ATAGAT","ACTAGTA","ATCGAATGTAG","ATCGTAATGAT"]
+#              0       1         2             3           #LEFT-RIGHT
+
+print(sequences[0])
+print(sequences[1])
+print(sequences[-2])
